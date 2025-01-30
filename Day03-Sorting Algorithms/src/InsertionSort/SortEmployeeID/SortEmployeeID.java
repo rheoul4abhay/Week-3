ff2745074f04637a919df0ebe90ebb7a0e8dfd09
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-01-31 00:11:32 +0530

    Added employee ID array sort using insertion sort

--- Day03-Sorting Algorithms/src/InsertionSort/SortEmployeeID/SortEmployeeID.java
+++ Day03-Sorting Algorithms/src/InsertionSort/SortEmployeeID/SortEmployeeID.java
@@ -1,4 +1,32 @@
 package InsertionSort.SortEmployeeID;
 
 public class SortEmployeeID {
+
+    public static void insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 0;i <= n-1;i++){
+            int j = i;
+            while(j > 0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int[] EmployeeIDArray = {13, 47, 23, 5, 18, 2};
+        System.out.print("ID's Before sort : ");
+        for(int id: EmployeeIDArray){
+            System.out.print(id + " ");
+        }
+
+        insertionSort(EmployeeIDArray);
+        System.out.print("\nID'S After sort : ");
+        for(int id: EmployeeIDArray){
+            System.out.print(id + " ");
+        }
+    }
 }
+
