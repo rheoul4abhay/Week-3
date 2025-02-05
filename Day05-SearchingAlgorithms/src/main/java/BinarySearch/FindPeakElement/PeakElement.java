3af39a9def82ed9f8e745659a3b0ff471bee84f3
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-02-05 12:15:24 +0530

    [Abhay Shrivastava] Add . program to find peak element in array

---
+++ Day05-SearchingAlgorithms/src/main/java/BinarySearch/FindPeakElement/PeakElement.java
@@ -0,0 +1,29 @@
+package BinarySearch.FindPeakElement;
+
+public class PeakElement {
+
+    public static int findPeakElement(int[] arr){
+        int low = 0;
+        int high = arr.length-1;
+
+        while(low < high){
+            int mid = low + (high-low)/2;
+            if((arr[mid] > arr[mid+1]) && (arr[mid] > arr[mid-1])){
+                return arr[mid];
+            }
+            else if(arr[mid] < arr[mid-1]){
+                high = mid-1;
+            }
+            else if(arr[mid] < arr[mid+1]){
+                low = mid+1;
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,1,3,5,6,4};
+        int result = PeakElement.findPeakElement(arr);
+        System.out.println("\nPeak element in array : " + result);
+    }
+}
