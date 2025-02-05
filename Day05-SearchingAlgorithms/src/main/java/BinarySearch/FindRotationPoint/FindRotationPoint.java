9cc8b0856103117dfbaa92d2800209234c1f01fa
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-02-05 10:55:19 +0530

    [Abhay Shrivastava] Add . Added rotationPoint program using binary Search approach

---
+++ Day05-SearchingAlgorithms/src/main/java/BinarySearch/FindRotationPoint/FindRotationPoint.java
@@ -0,0 +1,29 @@
+package BinarySearch.FindRotationPoint;
+
+public class FindRotationPoint {
+
+    public static int binarySearch(int[] arr){
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(arr[mid] > arr[right]){
+                //means the rotation point is in the right of mid, so we update left
+                left = mid+1;
+            }
+            else if(arr[mid] < arr[right]){
+                //means the smallest element is in the left half
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,9,1,2,3,4};
+        //our aim is to return te index of the smallest number in the array
+        int result = binarySearch(arr);
+        System.out.print("\nRotation point(smallest number index) is : " + result);
+    }
+}
